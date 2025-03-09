7b7d06c162119e5a1c36b33e0fd4512aeb6eb42b
PH Tools <phtools@bldgtyp.com>
2025-03-09 12:31:39 -0400

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+from pyairtable import Api
+from pyairtable.api.types import RecordDict
+
+load_dotenv()
+
+
+# Sackett Room Data
+# https://airtable.com/app64a1JuYVBs7Z1m/tblHB8tkzHso1tfYU/viw7REP61sRyUvbQV?blocks=hide
+AIRTABLE_API_KEY = str(os.getenv("AIRTABLE_API_KEY"))
+AIRTABLE_BASE_ID = str(os.getenv("AIRTABLE_BASE_ID"))
+AIRTABLE_TABLE_NAME = "tblapLjAFgm7RIllz"
+
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+    "https://ph-tools.github.io",
+    "https://bldgtyp.github.io",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/data")
+def get_data() -> list[RecordDict]:
+    api = Api(AIRTABLE_API_KEY)
+    print(f"using API key {AIRTABLE_API_KEY}")
+    print(f"Getting Data from {AIRTABLE_BASE_ID} | {AIRTABLE_TABLE_NAME}")
+    table = api.table(
+        base_id=AIRTABLE_BASE_ID,
+        table_name=AIRTABLE_TABLE_NAME
+    )
+    data = table.all()
+    print(f"Data: {data}")
+    return data
---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/data")
+      .then(response => setData(response.data || []))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Airtable Data</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Created Time</th>
+            <th>Category</th>
+            <th>Missing Datasheets?</th>
+            <th>Missing Specs?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(record => (
+            console.log(record),
+            <tr key={record.id}>
+              <td>{record.id}</td>
+              <td>{record.createdTime}</td>
+              <td>{record.fields.CATEGORY}</td>
+              <td>{record.fields.MISSING_DATASHEETS}</td>
+              <td>{record.fields.MISSING_SPECS}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
