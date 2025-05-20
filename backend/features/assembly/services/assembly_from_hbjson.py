1b1d9c0cdcd0567818467f2a72be4aff71fe1dfe
PH Tools <phtools@bldgtyp.com>
2025-05-20 13:51:17 -0400

    update

--- backend/features/assembly/services/assembly_from_hbjson.py
+++ backend/features/assembly/services/assembly_from_hbjson.py
@@ -8,7 +8,7 @@ from honeybee import dictutil as hb_dict_util
 from honeybee_energy.material.opaque import EnergyMaterial
 from honeybee_energy import dictutil as energy_dict_util
 from honeybee_energy.construction.opaque import OpaqueConstruction
-
+from honeybee_energy_ph.properties.materials.opaque import EnergyMaterialPhProperties
 
 from db_entities.assembly import Assembly, Layer, Segment, Material
 from db_entities.app import Project
@@ -63,7 +63,10 @@ async def get_material_from_hb_material(db: Session, hb_material: EnergyMaterial
 
 
 async def create_segment_from_hb_material(db: Session, hb_material: EnergyMaterial) -> Segment:
-    """Create a SegmentSchema from a Honeybee EnergyMaterial."""
+    """Create a Assembly-Layer-Segment from a Honeybee EnergyMaterial.
+    
+    Note: Changes are staged but NOT committed. Caller must commit.
+    """
     logger.info(f"create_segment_from_hb_material(hb_material={hb_material.display_name})")
 
     # Get the Segment-Material from the database
@@ -74,38 +77,46 @@ async def create_segment_from_hb_material(db: Session, hb_material: EnergyMateri
         order=0,
         width_mm=1000,
     )
-    db.add(new_segment)
-    db.commit()
-    db.refresh(new_segment)
-    
     new_segment.material = db_material
-    db.commit()
-    db.refresh(new_segment)
 
+    db.add(new_segment)
     return new_segment
 
 
 async def create_layer_from_hb_material(db: Session, hb_material: EnergyMaterial) -> Layer:
-    """Create a new LayerSchema from a Honeybee EnergyMaterial."""
-    logger.info(f"create_layer_from_hb_material(hb_material={hb_material.display_name})")
+    """Create a new Assembly-Layer from a Honeybee EnergyMaterial.
     
-    # Create a new Segment from the Honeybee EnergyMaterial
-    segment = await create_segment_from_hb_material(db, hb_material)
+    Note: Changes are staged but NOT committed. Caller must commit.
+    """
+    logger.info(f"create_layer_from_hb_material(hb_material={hb_material.display_name})")
 
-    # Create a new Layer object
+    segments: list[Segment] = []
+
+    # -- Deal with Mixed Materials
+    ph_props: EnergyMaterialPhProperties = getattr(hb_material.properties, "ph")
+    if ph_props.divisions.cell_count > 0:
+        
+        if ph_props.divisions.row_count > 1:
+            msg = f"Material {hb_material.display_name} has more than 1 row of Materials. This is not supported yet."
+            raise NotImplementedError(msg)
+        
+        for cell in ph_props.divisions.cells:
+            segments.append(await create_segment_from_hb_material(db, cell.material))
+    
+
+    # -- Create a new Segment from the Honeybee EnergyMaterial
+    segments.append(await create_segment_from_hb_material(db, hb_material))
+
+    # -- Create a new Layer object
     new_layer = Layer(
         order=0,
         thickness_mm=hb_material.thickness * 1000,
         segments=[],
     )
+    for new_segment in segments:
+        new_layer.segments.append(new_segment)
+    
     db.add(new_layer)
-    db.commit()
-    db.refresh(new_layer)
-
-    new_layer.segments.append(segment)
-    db.commit()
-    db.refresh(new_layer)
-
     return new_layer
 
 
@@ -113,11 +124,13 @@ async def create_assembly_from_hb_construction(db: Session, bt_number: int, hb_o
     """Create an AssemblySchema from a Honeybee OpaqueConstruction."""
     logger.info(f"create_assembly_from_hb_construction(hb_opaque_construction={hb_opaque_construction.display_name})")
     
+    # ------------------------------------------------------------------------------------------------------------------
     # -- Check if the project exists
     project = db.query(Project).filter_by(bt_number=bt_number).first()
     if not project:
         raise ValueError(f"Project with id {bt_number} not found in database.")
 
+    # ------------------------------------------------------------------------------------------------------------------
     # -- Get or Create the Assembly
     if assembly := db.query(Assembly).filter_by(name=hb_opaque_construction.display_name, project_id=project.id).first():
         logger.warning(f"Assembly with name {assembly.name} already exists for project {project.id}. Updating.")
@@ -129,25 +142,23 @@ async def create_assembly_from_hb_construction(db: Session, bt_number: int, hb_o
             layers=[],
         )
         db.add(assembly)
-        db.commit()
-        db.refresh(assembly)
 
-    # -- Build up all of the new Layers
+    # ------------------------------------------------------------------------------------------------------------------
+    # -- Build up all of the new Layers and Segments
     new_layers = []
-    for m in hb_opaque_construction.materials:
-        if not isinstance(m, EnergyMaterial):
-            logger.warning(f"Material {getattr(m, 'display_name', str(m))} [{type(m)=}] is not an EnergyMaterial. Ignoring.")
+    for hb_mat in hb_opaque_construction.materials:
+        if not isinstance(hb_mat, EnergyMaterial):
+            logger.warning(f"Material {getattr(hb_mat, 'display_name', str(hb_mat))} [{type(hb_mat)=}] is not an EnergyMaterial. Ignoring.")
             continue
-        new_layers.append(await create_layer_from_hb_material(db, m))
+        new_layers.append(await create_layer_from_hb_material(db, hb_mat))
 
+    # ------------------------------------------------------------------------------------------------------------------
     # -- Add the new layers to the Assembly
     assembly.remove_all_layers()
-    db.commit()
-    db.refresh(assembly)
-    
     for layer in new_layers:
         assembly.layers.append(layer)
 
+    # ------------------------------------------------------------------------------------------------------------------
     db.commit()
     db.refresh(assembly)
 
