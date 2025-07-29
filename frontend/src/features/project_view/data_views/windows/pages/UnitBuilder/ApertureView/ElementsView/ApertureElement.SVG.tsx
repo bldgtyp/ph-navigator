0c01525fa96997b57d91571fab86756f873b332d
PH Tools <phtools@bldgtyp.com>
2025-07-29 17:57:14 -0400

    update frame rendering

--- frontend/src/features/project_view/data_views/windows/pages/UnitBuilder/ApertureView/ElementsView/ApertureElement.SVG.tsx
+++ frontend/src/features/project_view/data_views/windows/pages/UnitBuilder/ApertureView/ElementsView/ApertureElement.SVG.tsx
@@ -3,10 +3,10 @@ import { ApertureElementSVGProps } from './types';
 const ApertureElementSVG: React.FC<ApertureElementSVGProps> = ({ height, width }) => {
     return (
         <svg width="100%" height="100%" viewBox={`0 0 ${width} ${height}`} preserveAspectRatio="none">
-            <rect x="0" y="0" width={width} height="10" fill="white" stroke="black" />
-            <rect x={width - 10} y="0" width="10" height={height} fill="white" stroke="black" />
-            <rect x="0" y={height - 10} width={width} height="10" fill="white" stroke="black" />
-            <rect x="0" y="0" width="10" height={height} fill="white" stroke="black" />
+            <rect x={width - 10} y="0" width="10" height={height} fill="white" stroke="black" /> {/* Right */}
+            <rect x="0" y="0" width="10" height={height} fill="white" stroke="black" /> {/* Left */}
+            <rect x="0" y="0" width={width} height="10" fill="white" stroke="black" /> {/* Top */}
+            <rect x="0" y={height - 10} width={width} height="10" fill="white" stroke="black" /> {/* Bottom */}
         </svg>
     );
 };
